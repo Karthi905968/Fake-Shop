8eaa1d9a322908ab6364ad3f28ea9ec237eeb4a7
TECH I.S <techi.s.@TECHs-MacBook-Air.local>
2024-03-13 14:30:45 +0530

    first commit

---
+++ src/containers/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='ui fixed menu'>
+        <div className='ui container center '>
+            <h2 style={{padding:'10px',fontSize:'20px',fontWeight:'bolder'}}>Fake Shop</h2>
+        </div>
+    </div>
+  )
+}
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import { productReducer,selectedProductReducer } from "./productReducer";
+
+export const reducers=combineReducers({
+     allProducts:productReducer,
+     products:selectedProductReducer,
+})
+
+
